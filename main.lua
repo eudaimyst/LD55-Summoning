be18b840b7271cb446238cca41f8d1121aeb87a0
skipgamer <tpilepich@gmail.com>
2024-04-14 16:11:37 +1000

    enemy spawning and movement

--- main.lua
+++ main.lua
@@ -4,9 +4,66 @@ local json = require "json"
 
 --constants
 
-local charScale = .4;
+local charScale = .5;
+local waveSpawnTimer = 20; --seconds for how often waves spawn
+local waveCount = 20
+local unitData = {
+	[1] = { attack = 20, hp = 80, attackSpeed = 0.5, moveSpeed = 1.2 },
+	[2] = { attack = 10, hp = 40, attackSpeed = 1, moveSpeed = 0.92 },
+	[3] = { attack = 11, hp = 40, attackSpeed = 1, moveSpeed = 0.93 },
+	[4] = { attack = 12, hp = 40, attackSpeed = 1, moveSpeed = 0.94 },
+	[5] = { attack = 13, hp = 40, attackSpeed = 1, moveSpeed = 0.95 },
+	[6] = { attack = 14, hp = 40, attackSpeed = 1, moveSpeed = 0.96 },
+	[7] = { attack = 15, hp = 40, attackSpeed = 1, moveSpeed = 0.97 },
+	[8] = { attack = 16, hp = 40, attackSpeed = 1, moveSpeed = 0.98 },
+	[9] = { attack = 17, hp = 40, attackSpeed = 1, moveSpeed = 0.99 },
+	[10] = { attack = 18, hp = 40, attackSpeed = 1, moveSpeed = 1 },
+	[11] = { attack = 20, hp = 60, attackSpeed = 1, moveSpeed = 1.4 },
+	[12] = { attack = 10, hp = 40, attackSpeed = 1, moveSpeed = 0.9 },
+	[13] = { attack = 0, hp = 100, attackSpeed = 0, moveSpeed = 0.3 }
+}
+local cardTypes = {
+
+	jack = {
+		frame = 5
+	},
+	queen = {
+		frame = 3
+	},
+	king = {
+		frame = 4
+	},
+	ace = {
+		frame = 6
+	},
+	number = {
+		frame = 1
+	}
+}
+--game variables
+local largestScreenSide = "y"
+local largestResolution = display.actualContentHeight
+if display.actualContentWidth > display.actualContentHeight then
+	largestScreenSide = "x"
+	largestResolution = display.actualContentWidth
+end
+
+local characters = {}
+local enemies = {}
 
 local mouseMode = "pick"
+local currentWave = 0
+local gameTimer = 0
+local waveTimer = 0
+
+local oldSystemTime = 0 --used to calculate deltaTime
+local deltaTime = 0
+
+local waveData = {}
+local function generateWaveData()
+
+end
+generateWaveData()
 
 local characterFrames = {
 	frames = {
@@ -62,29 +119,20 @@ for i = 2, 10 do
 	cardValues[i] = tostring(i);
 end
 
-local unitData = {
-	[1] = { attack = 20, hp = 80, attackSpeed = 0.5, moveSpeed = 1.2 },
-	[2] = { attack = 10, hp = 40, attackSpeed = 1, moveSpeed = 0.92 },
-	[3] = { attack = 11, hp = 40, attackSpeed = 1, moveSpeed = 0.93 },
-	[4] = { attack = 12, hp = 40, attackSpeed = 1, moveSpeed = 0.94 },
-	[5] = { attack = 13, hp = 40, attackSpeed = 1, moveSpeed = 0.95 },
-	[6] = { attack = 14, hp = 40, attackSpeed = 1, moveSpeed = 0.96 },
-	[7] = { attack = 15, hp = 40, attackSpeed = 1, moveSpeed = 0.97 },
-	[8] = { attack = 16, hp = 40, attackSpeed = 1, moveSpeed = 0.98 },
-	[9] = { attack = 17, hp = 40, attackSpeed = 1, moveSpeed = 0.99 },
-	[10] = { attack = 18, hp = 40, attackSpeed = 1, moveSpeed = 1 },
-	[11] = { attack = 20, hp = 60, attackSpeed = 1, moveSpeed = 1.4 },
-	[12] = { attack = 10, hp = 40, attackSpeed = 1, moveSpeed = 0.9 },
-	[13] = { attack = 0, hp = 100, attackSpeed = 0, moveSpeed = 0.3 }
-}
 local cards = {}
-local discardedCards = {}
 local deckOrder = {}
 local cardSlots = {}
 local handCards = {}
 local redCharacters = graphics.newImageSheet("assets/red_characters.png", characterFrames)
 local blackCharacters = graphics.newImageSheet("assets/black_characters.png", characterFrames)
 local suitSheet = graphics.newImageSheet("assets/suits.png", {
+	width = 64,
+	height = 64,
+	numFrames = 4,
+	sheetContentWidth = 256, -- width of original 1x size of entire sheet
+	sheetContentHeight = 64 -- height of original 1x size of entire sheet
+})
+local enemySheet = graphics.newImageSheet("assets/enemies.png", {
 	width = 64,
 	height = 64,
 	numFrames = 4,
@@ -100,24 +148,31 @@ local deck --assigned when deck is created
 local castle = display.newImageRect("assets/castle.png", 30, 30)
 castle.x, castle.y = display.actualContentWidth / 2, display.actualContentHeight / 3
 
-local cardTypes = {
+local waveTimerText = display.newText({
+	text = "next wave: 0",
+	x = 160,
+	y = display.screenOriginY + 14,
+	width = 100,
+	font = native.systemFont,
+	fontSize = 10,
+	align = "left" -- Alignment parameter
+})
+local currentWaveText = display.newText({
+	text = "current wave: 0",
+	x = 60,
+	y = display.screenOriginY + 14,
+	width = 100,
+	font = native.systemFont,
+	fontSize = 10,
+	align = "left" -- Alignment parameter
+})
 
-	jack = {
-		frame = 5
-	},
-	queen = {
-		frame = 3
-	},
-	king = {
-		frame = 4
-	},
-	ace = {
-		frame = 6
-	},
-	number = {
-		frame = 1
-	}
-}
+
+
+local function displayMessage(text, ms)
+	local messageText = display.newText(text, display.actualContentWidth / 2, 20, native.systemFont, 14)
+	timer.performWithDelay(ms, function() messageText:removeSelf() end);
+end
 
 local function generateCards()
 	local rank, suit = 1, 1
@@ -254,14 +309,27 @@ local function displayDeck()
 end
 displayDeck()
 
+local function moveUnit(unit, target) --called from units on tick function
+	local targetVector = { x = target.x - unit.x, y = target.y - unit.y }
+	local mag = math.abs(math.sqrt(math.pow(targetVector.x, 2) + math.pow(targetVector.y, 2)))
+	local unitVector = { x = targetVector.x / mag, y = targetVector.y / mag }
+	unit.x = unit.x + unitVector.x * deltaTime * unit.moveSpeed * 10
+	unit.y = unit.y + unitVector.y * deltaTime * unit.moveSpeed * 10
+end
+
 local function createCharacter(rank, suit, x, y)
-	local char = {
-		rank = rank,
-		suit = suit,
-		x = x,
-		y = y,
-		rect = display.newRect(x, y, 10, 10)
-	}
+	local char = display.newGroup()
+	char:scale(charScale, charScale);
+	char.x = x
+	char.y = y
+	char.rank = rank
+	char.suit = suit
+	for k, v in pairs(unitData[rank]) do
+		print(k, v)
+		char[k] = v
+	end
+	char.maxHP = char.hp
+	print(char.maxHP, char.hp)
 	if rank == 1 then
 		char.cardType = cardTypes.ace
 	elseif rank == 11 then
@@ -287,10 +355,59 @@ local function createCharacter(rank, suit, x, y)
 	else
 		paint.sheet = blackCharacters
 	end
-	char.rect.width = characterFrames.frames[char.frame].width * charScale
-	char.rect.height = characterFrames.frames[char.frame].height * charScale
-	char.rect.fill = paint
+	local frameWidth = characterFrames.frames[char.frame].width
+	local frameHeight = characterFrames.frames[char.frame].height
+	char.hpBG = display.newRect(char, 0, frameHeight / 2 + 2, frameWidth, 4)
+	char.hpBG:setFillColor(0, .15, 0)
+	char.hpBar = display.newRect(char, 0, frameHeight / 2 + 2, frameWidth, 4)
+	char.hpBar:setFillColor(0, 1, 0)
+	char.hpBar.update = function(self)
+		char.hpBar.xScale = char.hp / char.maxHP
+		char.hpBar.x = (-frameWidth / 2) * (1 - char.hpBar.xScale)
+	end
+	char.image = display.newRect(char, 0, 0, 10, 10)
+	char.image.width = frameWidth
+	char.image.height = frameHeight
+	char.image.fill = paint
 	char.tick = function(self)
+
+	end
+end
+
+local function createEnemy(level, suit, x, y)
+	local enemy = display.newGroup()
+	enemy.x, enemy.y = x, y
+	enemy.target = castle
+	enemy.moveSpeed = 1
+
+	local paint = {
+		type = "image",
+		frame = suit,
+		sheet = enemySheet
+	}
+	enemy.image = display.newRect(enemy, 0, 0, 10, 10)
+	enemy.image.fill = paint
+	enemy.tick = function(self)
+		moveUnit(self, self.target)
+	end
+	enemies[#enemies + 1] = enemy
+end
+
+local function spawnWave()
+	for i = 1, 10 do
+		local side = math.random(1, 4)
+		local suit = math.random(1, 4)
+		local offset = math.random(display.contentCenterX - largestResolution / 2,
+			display.contentCenterX + largestResolution / 2)
+		if side == 1 then
+			createEnemy(1, suit, offset, castle.y + largestResolution / 2)
+		elseif side == 2 then
+			createEnemy(1, suit, offset, castle.y + -largestResolution / 2)
+		elseif side == 3 then
+			createEnemy(1, suit, castle.x + largestResolution / 2, offset)
+		elseif side == 4 then
+			createEnemy(1, suit, castle.x - largestResolution / 2, offset)
+		end
 	end
 end
 
@@ -318,5 +435,26 @@ local function onMouseEvent(event)
 	end
 end
 
+local function onFrame(event)
+	local oldGameTimer = gameTimer
+	deltaTime = (system.getTimer() - oldSystemTime) / 1000
+	oldSystemTime = system.getTimer()
+	gameTimer = math.floor(system.getTimer() / 1000)
+	if oldGameTimer ~= gameTimer then
+		waveTimer = currentWave * waveSpawnTimer + 5 - gameTimer
+		print(gameTimer)
+		waveTimerText.text = "next wave: " .. waveTimer
+		if waveTimer < 1 then
+			currentWave = currentWave + 1
+			spawnWave()
+			currentWaveText.text = "current wave: " .. currentWave
+			displayMessage("wave " .. currentWave, 3000)
+		end
+	end
+	for i = 1, #enemies do
+		enemies[i]:tick()
+	end
+end
 -- Add the mouse event listener.
 Runtime:addEventListener("mouse", onMouseEvent)
+Runtime:addEventListener("enterFrame", onFrame)
